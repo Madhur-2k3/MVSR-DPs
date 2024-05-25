7e041afaf0e0e0ea8982de161694b23e54a0062e
Sridhar-Konidela <sridharkonidela@outlook.com>
2024-05-25 09:18:23 +0530

    Added images with numbers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  // const[rollNo,setRollNo]=useState('003')
+  const rollNumbers = [];
+  for (let i = 1; i <= 192; i++) {
+    rollNumbers.push(i.toString().padStart(3, '0')); // Converts numbers to '001', '002', ..., '190'
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className=' text-2xl text-center text-blue-400 bg-slate-900'>MVSR DP's</header>
+      <div className=' flex flex-wrap w-full justify-center bg-slate-700'>
+        {
+  
+          rollNumbers.map((rollNo)=>(
+            <div>
+            <img className=' w-32 m-3 rounded-full '
+          src={`http://results.mvsrec.edu.in/sbcmsstudent/Frm_StudentPhoto.aspx?StudentCode=${rollNo}`}/>
+          <h1 className=' text-center text-gray-300'>{rollNo}</h1>
+          </div>
+          ))
+      
+        }
+        
+      </div>
     </div>
   );
 }
